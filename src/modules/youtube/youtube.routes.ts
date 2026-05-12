f343b2d246e8d56f3d6586a03f4f2e25839d7e23
crimsonCarnival <agalvis@cotecnova.edu.co>
2026-05-11 20:51:51 -0500

    fix: remove preHandler requirement for active user on search route

--- src/modules/youtube/youtube.routes.ts
+++ src/modules/youtube/youtube.routes.ts
@@ -2,5 +2,5 @@ import type { FastifyInstance } from 'fastify';
 import { searchYoutube } from './youtube.controller.js';
 
 export default async function youtubeRoutes(fastify: FastifyInstance): Promise<void> {
-  fastify.get('/search', { preHandler: [fastify.requireActiveUser] }, searchYoutube);
+  fastify.get('/search', searchYoutube);
 }
