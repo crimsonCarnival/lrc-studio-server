94406fa5e789b6ee67bd35a5a838796c7b602b8a
crimsonCarnival <agalvis@cotecnova.edu.co>
2026-05-11 20:49:52 -0500

    feat: refactor loaders to improve user, upload, and lyrics fetching logic

--- src/graphql/loaders.ts
+++ src/graphql/loaders.ts
@@ -7,32 +7,104 @@ export const loaders: MercuriusLoaders = {
   Project: {
     user: {
       loader: async (queries: Array<{ obj: any }>, _context) => {
-        const ids = queries.map(({ obj }) => obj.userId);
-        const users = await User.find({ _id: { $in: ids } });
-        return ids.map((id: any) => users.find((u: any) => u._id.toString() === id.toString()));
+        const results = new Array(queries.length);
+        const toFetch: Array<{ id: any; index: number }> = [];
+
+        queries.forEach(({ obj }, i) => {
+          if (obj.user && (obj.user.id || obj.user._id)) {
+            results[i] = obj.user;
+          } else {
+            const id = obj.userId || obj.user; // fallback to obj.user if it's just an ID
+            if (id) toFetch.push({ id, index: i });
+            else results[i] = null;
+          }
+        });
+
+        if (toFetch.length > 0) {
+          const ids = toFetch.map(tf => tf.id);
+          const users = await User.find({ _id: { $in: ids } });
+          toFetch.forEach(tf => {
+            results[tf.index] = users.find(u => u._id.toString() === tf.id.toString()) || null;
+          });
+        }
+        return results;
       },
     },
     upload: {
       loader: async (queries: Array<{ obj: any }>, _context) => {
-        const ids = queries.map(({ obj }) => obj.uploadId);
-        const uploads = await Upload.find({ _id: { $in: ids } });
-        return ids.map((id: any) => uploads.find((u: any) => u._id.toString() === id?.toString()));
+        const results = new Array(queries.length);
+        const toFetch: Array<{ id: any; index: number }> = [];
+
+        queries.forEach(({ obj }, i) => {
+          if (obj.upload && (obj.upload.id || obj.upload._id)) {
+            results[i] = obj.upload;
+          } else {
+            const id = obj.uploadId || obj.upload;
+            if (id) toFetch.push({ id, index: i });
+            else results[i] = null;
+          }
+        });
+
+        if (toFetch.length > 0) {
+          const ids = toFetch.map(tf => tf.id);
+          const uploads = await Upload.find({ _id: { $in: ids } });
+          toFetch.forEach(tf => {
+            results[tf.index] = uploads.find(u => u._id.toString() === tf.id.toString()) || null;
+          });
+        }
+        return results;
       },
     },
     lyrics: {
       loader: async (queries: Array<{ obj: any }>, _context) => {
-        const ids = queries.map(({ obj }) => obj.lyricsId);
-        const lyricsList = await Lyrics.find({ _id: { $in: ids } });
-        return ids.map((id: any) => lyricsList.find((l: any) => l._id.toString() === id?.toString()));
+        const results = new Array(queries.length);
+        const toFetch: Array<{ id: any; index: number }> = [];
+
+        queries.forEach(({ obj }, i) => {
+          if (obj.lyrics && (obj.lyrics.id || obj.lyrics._id)) {
+            results[i] = obj.lyrics;
+          } else {
+            const id = obj.lyricsId || obj.lyrics;
+            if (id) toFetch.push({ id, index: i });
+            else results[i] = null;
+          }
+        });
+
+        if (toFetch.length > 0) {
+          const ids = toFetch.map(tf => tf.id);
+          const lyricsList = await Lyrics.find({ _id: { $in: ids } });
+          toFetch.forEach(tf => {
+            results[tf.index] = lyricsList.find(l => l._id.toString() === tf.id.toString()) || null;
+          });
+        }
+        return results;
       },
     },
   },
   Upload: {
     user: {
       loader: async (queries: Array<{ obj: any }>, _context) => {
-        const ids = queries.map(({ obj }) => obj.userId);
-        const users = await User.find({ _id: { $in: ids } });
-        return ids.map((id: any) => users.find((u: any) => u._id.toString() === id.toString()));
+        const results = new Array(queries.length);
+        const toFetch: Array<{ id: any; index: number }> = [];
+
+        queries.forEach(({ obj }, i) => {
+          if (obj.user && (obj.user.id || obj.user._id)) {
+            results[i] = obj.user;
+          } else {
+            const id = obj.userId || obj.user;
+            if (id) toFetch.push({ id, index: i });
+            else results[i] = null;
+          }
+        });
+
+        if (toFetch.length > 0) {
+          const ids = toFetch.map(tf => tf.id);
+          const users = await User.find({ _id: { $in: ids } });
+          toFetch.forEach(tf => {
+            results[tf.index] = users.find(u => u._id.toString() === tf.id.toString()) || null;
+          });
+        }
+        return results;
       },
     },
   },
