cc89334b4172769904181361ad0b39a93e94a192
crimsonCarnival <agalvis@cotecnova.edu.co>
2026-05-17 22:55:21 -0500

    feat: add resolvers for health, user, project, lyrics, upload, and settings

---
+++ src/graphql/resolvers/index.ts
@@ -0,0 +1,27 @@
+import { healthResolvers } from './health.resolvers.js';
+import { userResolvers } from './user.resolvers.js';
+import { projectResolvers } from './project.resolvers.js';
+import { lyricsResolvers } from './lyrics.resolvers.js';
+import { uploadResolvers } from './upload.resolvers.js';
+import { settingsResolvers } from './settings.resolvers.js';
+
+export const resolvers = {
+  Query: {
+    ...healthResolvers.Query,
+    ...userResolvers.Query,
+    ...projectResolvers.Query,
+    ...uploadResolvers.Query,
+    ...settingsResolvers.Query,
+  },
+  Mutation: {
+    ...userResolvers.Mutation,
+    ...projectResolvers.Mutation,
+    ...lyricsResolvers.Mutation,
+    ...uploadResolvers.Mutation,
+    ...settingsResolvers.Mutation,
+  },
+  User: userResolvers.User,
+  Project: projectResolvers.Project,
+  Lyrics: lyricsResolvers.Lyrics,
+  Upload: uploadResolvers.Upload,
+};
