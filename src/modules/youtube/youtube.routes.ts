00903b71229f7d648a5ed15a89322f2e15dfce75
crimsonCarnival <agalvis@cotecnova.edu.co>
2026-06-04 17:35:28 -0500

    feat: YouTube embeddability detection - batch status check in search, check-embed endpoint

--- src/modules/youtube/youtube.routes.ts
+++ src/modules/youtube/youtube.routes.ts
@@ -1,6 +1,7 @@
 import type { FastifyInstance } from 'fastify';
-import { searchYoutube } from './youtube.controller.js';
+import { searchYoutube, checkEmbed } from './youtube.controller.js';
 
 export default async function youtubeRoutes(fastify: FastifyInstance): Promise<void> {
   fastify.get('/search', searchYoutube);
+  fastify.get('/check-embed', checkEmbed);
 }
